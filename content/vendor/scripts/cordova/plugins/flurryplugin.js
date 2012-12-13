136b1203c85caf854f4b37a0783337f747f3fdc2
Jon Uy <jonxuy@gmail.com>
2012-12-12 19:16:44 -0500

    FlurryPlugin updates to support more calls

--- content/vendor/scripts/cordova/plugins/flurryplugin.js
+++ content/vendor/scripts/cordova/plugins/flurryplugin.js
@@ -3,16 +3,48 @@ FlurryPlugin = function() {
 };
 
 FlurryPlugin.prototype.pageView = function() {
-  cordova.exec(null,null,"FlurryPlugin","logPageView",[]);
+  cordova.exec(null, null, 'FlurryPlugin', 'logPageView',[]);
 };
 
 FlurryPlugin.prototype.logEvent = function(eventName) {
-  cordova.exec(null,null,"FlurryPlugin","logEvent",[{event: eventName}]);
+  cordova.exec(null, null, 'FlurryPlugin', 'logEvent', [{event: eventName}]);
 };
+
+FlurryPlugin.prototype.logEventTimed = function(eventName) {
+  cordova.exec(null, null, 'FlurryPlugin', 'logEventTimed', [{event: eventName}]);
+}
 
 FlurryPlugin.prototype.logEventWithParameters = function(eventName, params) {
   cordova.exec(null, null, 'FlurryPlugin', 'logEventWithParameters', [{event: eventName, params: params}]);
 };
+
+FlurryPlugin.prototype.logEventWithParametersTimed = function(eventName, params) {
+  cordova.exec(null, null, 'FlurryPlugin', 'logEventWithParameters', [{event: eventName, params: params}]);
+};
+
+FlurryPlugin.prototype.endTimedEventWithParameters = function(eventName, params) {
+  cordova.exec(null, null, 'FlurryPlugin', 'endTimedEventWithParameters', [{event: eventName, params: params}]);
+};
+
+FlurryPlugin.prototype.logError = function(error, message) {
+  cordova.exec(null, null, 'FlurryPlugin', 'logError', [{error: error, message: message}]);
+}
+
+FlurryPlugin.prototype.setAge = function(age) {
+  cordova.exec(null, null, 'FlurryPlugin', 'setAge', [{age: age}]);
+}
+
+FlurryPlugin.prototype.setGender = function(gender) {
+  cordova.exec(null, null, 'FlurryPlugin', 'setGender', [{gender: gender}]);
+}
+
+FlurryPlugin.prototype.setUserID = function(user_id) {
+  cordova.exec(null, null, 'FlurryPlugin', 'setUserID', [{userid: user_id}]);
+}
+
+FlurryPlugin.prototype.setLocation = function(lat, lon, h_acc, v_acc) {
+  cordova.exec(null, null, 'FlurryPlugin', 'setLocation', [{latitude: lat, longitude: lon, horizontal_acc: h_acc, vertical_acc: v_acc}]);
+}
 
 cordova.addConstructor(function() {
 
